19f43a607cc3dc8e8e4669f315e5be81682ec13c
nathanieldorn <nathaniel.svc@pm.me>
2025-11-10 20:12:51 -0500

    Completed random walk

--- randomwalk_plot.py
+++ randomwalk_plot.py
@@ -1,15 +1,48 @@
 import matplotlib.pyplot as pyplot
+
 from randomwalks import RandomWalk
 
 
 def walk_about():
-    # create a random walk
-    random_walk = RandomWalk()
-    random_walk.fill_walk()
+    """Plot the walk, prompt for file save, and prompt to run again."""
 
-    # plot the walk
-    pyplot.style.use('dark_background')
-    fig, ax = pyplot.subplots()
-    ax.scatter(random_walk.x_values, random_walk.y_values, color=(0, 0.8, 0), s=15)
-    ax.set_aspect('equal')
-    pyplot.show()
+    while True:
+        # create a random walk
+        random_walk = RandomWalk(num_points=100000)
+        random_walk.fill_walk()
+
+        # plot the walk
+        pyplot.style.use("dark_background")
+        fig, ax = pyplot.subplots(figsize=(15, 9), dpi=128)
+        point_numbers = range(random_walk.num_points)
+        ax.scatter(
+            random_walk.x_values,
+            random_walk.y_values,
+            c=point_numbers,
+            cmap=pyplot.cm.Greens,
+            edgecolors="none",
+            s=15,
+        )
+
+        # re-color first and last point
+        ax.scatter(0, 0, color=(0.9, 0.6, 0, 0.75), edgecolors="none", s=75)
+        ax.scatter(
+            random_walk.x_values[-1],
+            random_walk.y_values[-1],
+            color=(0.9, 0.6, 0, 0.75),
+            edgecolor="none",
+            s=75,
+        )
+
+        ax.set_aspect("equal")
+
+        # prompt for file save and re-run
+        save_copy = input("Would you like to save a copy? (y/n): ")
+        if save_copy == "y":
+            pyplot.savefig("random_walk_100k.png", dpi="figure", bbox_inches="tight")
+
+        pyplot.show()
+
+        keep_running = input("Make another walk? (y/n): ")
+        if keep_running == "n":
+            break
